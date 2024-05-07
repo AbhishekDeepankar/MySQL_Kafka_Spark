d1cad18ecd24cb69364c37b11e610140d306dae7
AbhishekDeepankar <abhi.deepankar@gmail.com>
2024-05-06 17:51:51 -0700

    Update constants.py

--- kafka_mysql/Config/constants.py
+++ kafka_mysql/Config/constants.py
@@ -1,6 +1,6 @@
-pipeln_topic = ''
-pipeln_db = ''
-pipeln_broker = []
-pipeln_db_host = ''
-pipeln_table1 = ''
-pipeln_bookmark_tbl = ''
+pipeln_topic = ''        # Kafka topic
+pipeln_db = ''           # MySQL database
+pipeln_broker = []       # Kafka broker list
+pipeln_db_host = ''      # Mysql database host
+pipeln_table1 = ''       # Mysql table
+pipeln_bookmark_tbl = '' # Mysql bookmark table
