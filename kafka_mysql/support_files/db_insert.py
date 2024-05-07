38f3e435692bc08a87cc600fb6afbf757249c66f
AbhishekDeepankar <abhi.deepankar@gmail.com>
2024-05-07 16:54:41 -0700

    Update db_insert.py

--- kafka_mysql/support_files/db_insert.py
+++ kafka_mysql/support_files/db_insert.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     engine = get_mysql_engine(c.mysql_user1, c.mysql_user_password1,
                               host=cn.pipeln_db_host, database=cn.pipeln_db)
 
-    df = pd.read_csv("D:\\Datasets\\global_electricity_production_data.csv")
+    df = pd.read_csv("file_path")
     df['created_on'] = current_timestamp
     df['updated_on'] = current_timestamp
-    df.to_sql('ELECTRICITY_PRODUCTION', con=engine, if_exists='append', index=False)
+    df.to_sql('table_name', con=engine, if_exists='append', index=False)
     # df = df.head(1)
     print(df)
     engine.dispose()
