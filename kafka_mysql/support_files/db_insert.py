b9c1dbb3061bb31b7213fd2c50472f4358c49bc2
Abhishek Deepankar <abhi.deepankar@gmail.com>
2024-05-08 14:57:23 -0700

    comments

--- kafka_mysql/support_files/db_insert.py
+++ kafka_mysql/support_files/db_insert.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     engine = get_mysql_engine(c.mysql_user1, c.mysql_user_password1,
                               host=cn.pipeln_db_host, database=cn.pipeln_db)
 
-    df = pd.read_csv("file_path")
-    df['created_on'] = current_timestamp
-    df['updated_on'] = current_timestamp
-    df.to_sql('table_name', con=engine, if_exists='append', index=False)
+    insert_df = pd.read_csv("file_path")
+    insert_df['created_on'] = current_timestamp
+    insert_df['updated_on'] = current_timestamp
+    insert_df.to_sql('table_name', con=engine, if_exists='append', index=False)
     # df = df.head(1)
-    print(df)
+    print(insert_df)
     engine.dispose()
     
